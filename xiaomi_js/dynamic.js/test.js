53f32f6210e880b4597abcd8ab4a6ae78019a186
zq <zhaoqin_yin@163.com>
2020-05-05 21:44:16 +0800

    done some js interactions on icon-nav section, and it needs more work to perfect it

--- xiaomi_js/dynamic.js/test.js
+++ xiaomi_js/dynamic.js/test.js
@@ -2,5 +2,25 @@
 *js部分
 *vanilla js script
 */
-
-console.log('test is good')
+// 获得 drop-content里面的 img数组
+var imgUrlArray = [
+    ['imgs/phones/phone1.png','imgs/phones/phone2.png','imgs/phones/phone3.png','imgs/phones/phone4.png','imgs/phones/phone5.png','imgs/phones/phone6.png',],
+    ['imgs/hongmi/hongmi1.jpg','imgs/hongmi/hongmi2.png','imgs/hongmi/hongmi3.jpg','imgs/hongmi/hongmi4.jpg','imgs/hongmi/hongmi5.png','imgs/hongmi/hongmi6.jpg',],
+    ['imgs/tvs/tv1.jpg','imgs/tvs/tv2.jpg','imgs/tvs/tv3.jpg','imgs/tvs/tv4.jpg','imgs/tvs/tv5.png','imgs/tvs/tv6.jpg',],
+    ['imgs/laptops/lp1.jpg','imgs/laptops/lp2.png','imgs/laptops/lp3.png','imgs/laptops/lp4.png','imgs/laptops/lp5.png','imgs/laptops/lp6.png',]
+]
+var imgEles = document.getElementById('drop-content').querySelectorAll('img')
+// 获得nav-text元素数组
+var navTexts = document.getElementById('drop-yes').getElementsByClassName('nav-text')
+// 遍历imgs
+for(var i = 0;i<imgEles.length;i++){
+// 获取mouseup nav-text 事件
+// textInex 和 i 的数量相同，应该同步
+    (function(textIndex){
+        navTexts[textIndex].onmouseover = function(){
+            for(var j = 0; j < imgEles.length;j++){
+                imgEles[j].setAttribute('src',imgUrlArray[textIndex][j])
+            }
+        }
+    })(i)
+}
